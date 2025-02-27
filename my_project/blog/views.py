040c720abab77beac758c74551325f3feb75cb29
joola10 <joola10@morgan.edu>
2025-02-27 06:49:28 -0500

    Add files via upload

---
+++ my_project/blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+
+def index(request):
+    return HttpResponse(" This is my first url")
+
+def specific(request):
+    return HttpResponse("list1")
+
+def article(request,article_id):
+    return render(request,'blog/index.html',{'article_id':article_id})
